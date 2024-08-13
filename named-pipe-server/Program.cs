ed86f86f325a6c8f6bab7e40fa517ef22bcb2bff
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2024-08-13 15:03:12 +0600

    Adjust for Windows

--- named-pipe-server/Program.cs
+++ named-pipe-server/Program.cs
@@ -8,9 +8,11 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        string pipeName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? @"\\.\pipe\node_named_pipe"
-            : "/tmp/node_named_pipe";
+        var localPipeName = "node_named_pipe";
+        var pipeName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? @"\\.\pipe\"
+            : "/tmp/";
+        pipeName += localPipeName;
 
         if (!File.Exists(pipeName))
         {
@@ -18,27 +20,28 @@ class Program
             return;
         }
 
-        using (var client = new NamedPipeClientStream(pipeName))
+        await using var client = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+            new NamedPipeClientStream(".", localPipeName, PipeDirection.InOut, PipeOptions.Asynchronous) :
+            new NamedPipeClientStream(pipeName);
+        try
         {
-            try
-            {
-                await client.ConnectAsync();
-                Console.WriteLine("Connected to the named pipe.");
+            //Hangs here
+            await client.ConnectAsync();
+            Console.WriteLine("Connected to the named pipe.");
 
-                using var writer = new StreamWriter(client);
-                var reader = new StreamReader(client);
-                writer.AutoFlush = true;
-                string message = "Hello from .NET 8!";
-                await writer.WriteLineAsync(message);
-                Console.WriteLine($"Sent: {message}");
+            await using var writer = new StreamWriter(client);
+            var reader = new StreamReader(client);
+            writer.AutoFlush = true;
+            string message = "Hello from .NET 8!";
+            await writer.WriteLineAsync(message);
+            Console.WriteLine($"Sent: {message}");
 
-                string response = await reader.ReadLineAsync();
-                Console.WriteLine($"Received: {response}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
+            string response = await reader.ReadLineAsync();
+            Console.WriteLine($"Received: {response}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
         }
     }
 }
