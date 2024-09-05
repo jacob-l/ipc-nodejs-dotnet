37b2a553bf0e5118c6777c1a924d947e5595246a
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2024-09-05 11:34:36 +0600

    Add message pack example

--- named-pipe-server/Program.cs
+++ named-pipe-server/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.IO;
+﻿using MessagePack;
 using System.IO.Pipes;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+
+[Union(0, typeof(Message1))]
+[Union(1, typeof(Message2))]
+[MessagePackObject(keyAsPropertyName: true)]
+public abstract class Message
+{
+    private const string CurrentVersion = "2.0";
+
+    public string Version { get; set; } = CurrentVersion;
+
+    public Guid Id { get; set; }
+}
+
+[MessagePackObject(keyAsPropertyName: true)]
+public class Message1 : Message
+{
+    public string MetaData1 { get; set; }
+}
+
+[MessagePackObject(keyAsPropertyName: true)]
+public class Message2 : Message
+{
+    public string MetaData2 { get; set; }
+}
+
 
 class Program
 {
-    static async Task Main(string[] args)
+    private static int ReadInt32(Stream stream)
+    {
+        byte[] bytes = new byte[4];
+        _ = stream.Read(bytes, 0, 4);
+        if (!BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(bytes);
+        }
+        return BitConverter.ToInt32(bytes, 0);
+    }
+
+    private static Message ReadMessage(Stream stream)
+    {
+        int len = ReadInt32(stream);
+        Console.WriteLine("Length - " + len);
+        byte[] inBuffer = new byte[len];
+        _ = stream.Read(inBuffer, 0, len);
+
+        return MessagePack.MessagePackSerializer.Deserialize<Message>(inBuffer);
+    }
+
+    static void Main(string[] args)
     {
         var localPipeName = "node_named_pipe";
         var pipeName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
@@ -14,30 +58,27 @@ class Program
             : "/tmp/";
         pipeName += localPipeName;
 
+        /*
         if (!File.Exists(pipeName))
         {
             Console.WriteLine($"The named pipe {pipeName} does not exist.");
             return;
-        }
+        }*/
 
-        await using var client = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-            new NamedPipeClientStream(".", localPipeName, PipeDirection.InOut, PipeOptions.Asynchronous) :
-            new NamedPipeClientStream(pipeName);
+        var client = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+            new NamedPipeClientStream(".", localPipeName, PipeDirection.In) :
+            new NamedPipeClientStream(".", pipeName, PipeDirection.In);
         try
         {
-            //Hangs here
-            await client.ConnectAsync();
+            client.Connect();
             Console.WriteLine("Connected to the named pipe.");
 
-            await using var writer = new StreamWriter(client);
-            var reader = new StreamReader(client);
-            writer.AutoFlush = true;
-            string message = "Hello from .NET 8!";
-            await writer.WriteLineAsync(message);
-            Console.WriteLine($"Sent: {message}");
-
-            string response = await reader.ReadLineAsync();
-            Console.WriteLine($"Received: {response}");
+            var deserializedMessage = ReadMessage(client);
+            Console.WriteLine($"Received: {deserializedMessage.Version} {deserializedMessage.Id}");
+            if (deserializedMessage is Message1 msg1)
+            {
+                Console.WriteLine($"MetaData1: {msg1.MetaData1}");
+            }
         }
         catch (Exception ex)
         {
--- program.js
+++ program.js
@@ -1,6 +1,40 @@
 const net = require('net');
 const os = require('os');
 const fs = require('fs');
+const msgpack = require('@msgpack/msgpack');
+const uuidv4 = require('uuid').v4;
+
+class Message {
+    Version;
+    Id;
+
+    constructor() {
+        this.Version = "3.0";
+        this.Id = uuidv4();
+    }
+}
+
+// Derived class Message1
+class Message1 extends Message {
+    MetaData1;
+
+    constructor(metaData1) {
+        super();
+        this.MetaData1 = metaData1;
+    }
+}
+
+// Derived class Message2
+class Message2 extends Message {
+    MetaData2;
+
+    constructor(metaData2) {
+        super();
+        this.MetaData2 = metaData2;
+    }
+}
+
+const message = new Message1("Hi From Message 1");
 
 const PIPE_PATH = os.platform() === 'win32' ? '\\\\.\\pipe\\node_named_pipe' : '/tmp/node_named_pipe';
 
@@ -8,13 +42,44 @@ if (fs.existsSync(PIPE_PATH)) {
     fs.unlinkSync(PIPE_PATH);
 }
 
-const server = net.createServer((socket) => {
-    console.log('Client connected');
-    socket.on('data', (data) => {
-        console.log(`Received: ${data}`);
-        socket.write(`Echo: ${data}`);
-    });
+function writeInt32(socket, value) {
+    const buffer = Buffer.alloc(4);
+    buffer.writeInt32LE(value, 0);
+    console.log('Starting write length');
+    socket.write(buffer);
+}
 
+// Function to serialize a message with type information
+function serializeMessage(message) {
+    let type;
+    let messageData;
+
+    console.log(message);
+    if (message instanceof Message1) {
+        type = 0; // Corresponds to [Union(0, typeof(Message1))]
+        messageData = { Version: message.Version, Id: message.Id, MetaData1: message.MetaData1 };
+    } else if (message instanceof Message2) {
+        type = 1; // Corresponds to [Union(1, typeof(Message2))]
+        messageData = { MetaData2: message.MetaData2, Version: message.Version, Id: message.Id };
+    } else {
+        throw new Error("Unknown message type");
+    }
+
+    console.log(messageData);
+    // Pack the type and the message data together
+    const packedMessage = msgpack.encode([type, messageData]);
+
+    return packedMessage;
+}
+
+const server = net.createServer((socket) => {
+    console.log('Client connected ' + new Date());
+
+    const outBuffer = serializeMessage(message);
+	const len = outBuffer.length;
+
+	writeInt32(socket, len);
+	socket.write(outBuffer);
     socket.on('end', () => {
         console.log('Client disconnected');
     });
